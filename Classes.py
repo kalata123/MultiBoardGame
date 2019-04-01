e304d86cec6f66ebc6444ebc0c694eb52a5233cb
Kaloyan2021 <kaloyan.elsys2021@gmail.com>
2019-04-01 16:42:39 +0300

    First phase of game 1 - DONE

--- Classes.py
+++ Classes.py
@@ -1,44 +1,27 @@
 # File only for classes
 
-class Player:
+class Player():
 
-    owned_pos = []
-
-    def __init__(self, start_lives = 3, start_money = 1000, position = None):
-        self.position = position
+    def __init__(self, name, start_lives = 3, start_money = 1000, position = None):
+        self.name = name
+        self.curr_position = position
         self.lives = start_lives
         self.money = start_money
+        self.owned_pos = []
 
     def __str__(self):
-        return "Player on {} position with {} lives and {}$".format(self.position, self.lives, self.money)
+        return '''Player {:6} on position {:2} with {} lives and {:3}$ and \
+{} positions'''.format(self.name, self.curr_position, \
+        self.lives, self.money, self.owned_pos)
 
-    def PAY(self, money_to_pay):
-        self.money -= money_to_pay
+    def ADD_POSITION(self, new_pos, money_to_pay):
+        if self.money >= money_to_pay:
+            self.money = self.money - money_to_pay
+            self.owned_pos.append(new_pos)
+            self.curr_position = new_pos
+            return 1
+        else:
+            return -1
 
     def LOSE_HP(self, hp_to_lose = 1):
         self.lives -= hp_to_lose
-
-    def NEW_POSITION(self, new_pos):
-        self.position = new_pos
-
-class Board:
-
-    def __init__(self, jacks_pins, buttons_pins, neighbours = None):
-        self.jacks_pins = jacks_pins
-        self.buttons_pins = buttons_pins
-        self.neighbours = neighbours
-
-    def ADD_NEIGHBOUR(self, n_pos):
-        # n_pos == neightbour position
-        # appends it to the table if its not in it
-        if n_pos not in self.neighbours:
-            self.neighbours.append(n_pos)
-        else:
-            pass
-
-    def REMOVE_NEIGHBOUR(self, n_pos):
-        # removes neigh_position if in neighbours
-        if n_pos in self.neighbours:
-            self.neighbours.remove(i)
-        else:
-            pass
--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 import gpiozero
 import time
-from Classes import *
+from Classes import Player
 import random
+
+
+p1 = Player("Red",3,600,0)
+p2 = Player("Yellow",3,600,1)
+p3 = Player("White",3,600,2)
+p4 = Player("Black",3,600,3)
+total_players = 4
 
 jacks_repr = {
     13:"I",
@@ -9,20 +16,21 @@ jacks_repr = {
     6:"III",
     8:"IV",
     3:"V",
-    1:"VI",
+    # 1:"VI", # трябва да се оправи
     20:"VII",
     27:"VIII",
     19:"IX",
     24:"X",
-    23:"XI",
+    # 23:"XI",
     25:"XII",
     16:"XIII",
     # 21:"XIV", # на късо свързано и винаги е pushed state - hardware error
     12:"XV",
     5:"XVI",
-    26:"BLB",
-    2:"BB",
-    17:"RB"
+    # Buttons
+    17:"Black Button",
+    2:"Blue Button",
+    26:"Red Button"
 
 }
 
@@ -32,12 +40,12 @@ jacks_pins = [
     6,
     8,
     3,
-    1,
+    # 1,
     20,
     27,
     19,
     24,
-    23,
+    # 23,
     25,
     16,
     # 21, # на късо свързано и винаги е pushed state - hardware error
@@ -52,163 +60,115 @@ buttons_pins = [
 ]
 
 neighbours = []
-
+# Functions for beggining
 def START_CHECK():
     # checks if the board is clear from palms
     j = 0
-    while j is not 14:
+    while j is not len(jacks_repr.keys()) - 3:
         j = 0
-        print("i=0")
         for i in jacks_pins:
             try:
                 if gpiozero.Button(i).is_pressed:
-                    if gpiozero.Button(i).is_held:
-                        print("it is held")
                     print("Take out {} palm".format(jacks_repr[i]))
-                    break
-                else:
-                    print(" i = {}".format(i))
-                    j += 1
+                    gpiozero.Button(i).wait_for_release()
+                j += 1
             except RuntimeError as e:
                 print(e)
 
 def DICE_1():
+    # returns a random pin number from jacks_pins list
     a = random.choice(jacks_pins)
-    if a in neighbours:
-        return DICE_1()
-    else:
+    if a not in neighbours:
         return a
+    return DICE_1()
 
-def pl1(pos, lives=3, money=300):
-    global p1
+def BUY(player, pos, money_to_pay = 100):
+    # gives <pos> over to <player> and
+    # changes <player>'s curr_position
+    if player == 0:
+        return p1.ADD_POSITION(pos, money_to_pay)
+    elif player == 1:
+        return p2.ADD_POSITION(pos, money_to_pay)
+    elif player == 2:
+        return p3.ADD_POSITION(pos, money_to_pay)
+    elif player == 3:
+        return p4.ADD_POSITION(pos, money_to_pay)
 
-    p1 = Player(lives, money, pos)
-    if pos not in p1.owned_pos:
-        p1.owned_pos.append(pos)
-        print("adding {}".format(p1.owned_pos))
-    return p1
-def pl2(pos, lives=3, money=300):
-    global p2
-    p2 = Player(lives, money, pos)
-    p2.owned_pos.append(pos)
-    return p2
-def pl3(pos, lives=3, money=300):
-    global p3
-    p3 = Player(lives, money, pos)
-    p3.owned_pos.append(pos)
-    return p3
-def pl4(pos, lives=3, money=300):
-    global p4
-    p4 = Player(lives, money, pos)
-    p4.owned_pos.append(pos)
-    return p4
-def INIT_PL(player, pos):
-    # initialises a player
-    switcher = {
-        1:pl1(pos),
-        2:pl2(pos),
-        3:pl3(pos),
-        4:pl4(pos)
+def LAST_POS(player):
+    switch = {
+        0:p1.curr_position,
+        1:p2.curr_position,
+        2:p3.curr_position,
+        3:p4.curr_position
     }
+    return switch[player]
 
+def GO_BACK(player):
+    print("You don't have enough money to buy this position\nPlease go back to {}"\
+    .format(jacks_repr[LAST_POS(player)]))
+    if gpiozero.Button(LAST_POS(player)).wait_for_press(20):
+        return 1
+    else:
+        return 0
 
-# START_CHECK()
+def PLAYER_COLOR(player):
+    switcher = {
+        0:"Red",
+        1:"Yellow",
+        2:"White",
+        3:"Black"
+    }
+    return switcher[player]
 
-place_to_go = DICE_1()
-print("Go to {}".format(place_to_go))
-gpiozero.Button(place_to_go).wait_for_press()
-INIT_PL(1, place_to_go)
-print("printit {} {}".format(p1, p1.owned_pos))
-for i in p1.owned_pos:
-    print(i)
+def start():
+    START_CHECK()
+    global total_players
+    while len(neighbours) < 12:
+        for i in range(total_players):
+            if (len(neighbours) >= 12):
+                break
+            try:
+                place_to_go = DICE_1()
+                print("{} Go to {}".format(PLAYER_COLOR(i),jacks_repr[place_to_go]))
+                time.sleep(0.4)
+                if gpiozero.Button(place_to_go).wait_for_press(20):
+                    time.sleep(0.4)
+                    if BUY(i, place_to_go) != -1:
+                        neighbours.append(place_to_go) # so that DICE doesn't give u same pos twice
+                    else:
+                        while not GO_BACK(i):
+                            GO_BACK(i)
 
+                else:
+                    print("Error else - start anew")
+                    exit(1)
+            except TypeError as e:
+                print(p1)
+                print(p2)
+                print(p3)
+                print(p4)
+                print(e)
+                continue
+# End of Functions for beggining
+def CURR_STATE():
+    print('''This is the current state:\n\
+    Red    - on {:3} with {}$ and {} lives\n\
+    Yellow - on {:3} with {}$ and {} lives\n\
+    White  - on {:3} with {}$ and {} lives\n\
+    Black  - on {:3} with {}$ and {} lives'''.format(\
+    jacks_repr[p1.curr_position], p1.money, p1.lives,\
+    jacks_repr[p2.curr_position], p2.money, p2.lives,\
+    jacks_repr[p3.curr_position], p3.money, p3.lives,
+    jacks_repr[p4.curr_position], p4.money, p4.lives))
+# Start of Functions for main program
+def DICE_2():
+    return random.choice([1, 2, 3, 4, 5, 6])
 
+def MAIN():
+    pass
 
-
-
-# neighbours = []
-#
-# jacks_pins = [
-#     13,
-#     # 7, # на късо свързано и винаги е pushed state - hardware error
-#     6,
-#     8,
-#     3,
-#     1,
-#     20,
-#     27,
-#     19,
-#     24,
-#     23,
-#     25,
-#     16,
-#     # 21, # на късо свързано и винаги е pushed state - hardware error
-#     12,
-#     5
-# ]
-#
-# buttons_pins = [
-#     26,
-#     2,
-#     17
-# ]
-#
-# bad_jacks = [
-#     6,
-#     8,
-#     1,
-#     20,
-#     24,
-#     25,
-#     12,
-#     5
-# ]
-#
-# b = Classes.Board(jacks_pins, buttons_pins, neighbours)
-# p1 = Classes.Player(3, 200)
-# p2 = Classes.Player(3, 200)
-# p3 = Classes.Player(3, 200)
-# p4 = Classes.Player(3, 200)
-#
-# def CHECK_NEIGH():
-#     # checks the board jacks,
-#     # and if all in are in neighbour table does nothing
-#     # else adds it.
-#
-#     for i in jacks_pins:
-#         if gpiozero.Button(i).is_pressed:
-#             b.ADD_NEIGHBOUR(i)
-#         else:
-#             b.REMOVE_NEIGHBOUR(i)
-# def SET_PL_to_POS(neigh_table):
-#     p1.NEW_POSITION(neigh_table[0])
-#     p2.NEW_POSITION(neigh_table[1])
-#     p3.NEW_POSITION(neigh_table[2])
-#     p4.NEW_POSITION(neigh_table[3])
-# def DICE():
-#     print("The dice has been diced")
-#     a = random.choice(jacks_pins)
-#     if a in neighbours:
-#         DICE()
-#     else:
-#         print("move to {}".format(a))
-#         return a
-#
-# CHECK_NEIGH()
-# # SET_PL_to_POS(neighbours)
-# while True:
-#     if gpiozero.Button(2).is_pressed:
-#         a = DICE()
-#         print("Butt was pressed and dice is {}".format(a))
-#         gpiozero.Button(a).wait_for_press()
-#         p1.NEW_POSITION(a)
-#     if p1.position in bad_jacks:
-#         print("this is gonna be bad")
-#     else:
-#         print("You are safe")
-#     # print(p1)
-#     # print(p2)
-#     # print(p3)
-#     # print(p4)
-#     # print(neighbours)
-#     #print("none for now")
+if __name__ == "__main__":
+    gpiozero.Button(2).wait_for_press()
+    start()
+    CURR_STATE()
+    MAIN()
